d1eb4b41b3a849433ac2b54d88d87ec84570c40a
Maarten Suetens <maarten.suetens@phpro.be>
2016-04-08 15:51:27 +0200

    added amis for all regions

--- variables.tf
+++ variables.tf
@@ -15,13 +15,29 @@ variable "source_ip" {
 variable "aws_amis" {
   default = {
     eu-central-1 = "ami-e2df388d"
+    eu-west-1 = "ami-31328842"
     us-east-1 = "ami-08111162"
+    us-west-1 = "ami-1b0f7d7b"
+    us-west-2 = "ami-c229c0a2"
+    ap-northeast-1 = "ami-f80e0596"
+    ap-northeast-2 = "ami-6598510b"
+    ap-southeast-1 = "ami-e90dc68a"
+    ap-southeast-2 = "ami-f2210191"
+    sa-east-1 = "ami-1e159872"
   }
 }
 
 variable "aws_nat_amis" {
   default = {
     eu-central-1 = "ami-0b322e67"
+    eu-west-1 = "ami-14913f63"
     us-east-1 = "ami-184dc970"
+    us-west-1 = "ami-0d087a6d"
+    us-west-2 = "ami-030f4133"
+    ap-northeast-1 = "ami-03cf3903"
+    ap-northeast-2 = "ami-0199506f"
+    ap-southeast-1 = "ami-1a9dac48"
+    ap-southeast-2 = "ami-0154c73b"
+    sa-east-1 = "ami-22169b4e"
   }
 }
